ba28a1b2c92659d90290e433f26929621f6a9757
Christoffer Noring <noringc@gmail.com>
2018-02-04 16:08:03 +0000

    updating demo effects

--- Chapter9/DemoEffects/src/app/app.module.ts
+++ Chapter9/DemoEffects/src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { EffectsModule } from "@ngrx/effects";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { StoreModule } from "@ngrx/store";
+import { ProductModule } from "./product/product.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    EffectsModule,
+    StoreModule.forRoot({}),
+    EffectsModule.forRoot([]),
+    ProductModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
