27e12438b1301e4d20c4f67de7d9d35aafb24642
Christoffer Noring <noringc@gmail.com>
2018-03-08 00:54:29 +0000

    adding some and removing some other

--- Chapter7/error-retry.js
+++
@@ -1,8 +0,0 @@
-import Rx from "rxjs/Rx";
-
-let stream = Rx.Observable.create(observer => {
-  observer.next(1);
-  observer.error("err");
-})
-  .retry(3)
-  .subscribe(data => console.log(data));
