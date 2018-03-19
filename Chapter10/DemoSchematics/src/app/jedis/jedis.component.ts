c29ce2cc96cc040e1c848b1eadad69f3cf07b4cf
Christoffer Noring <noringc@gmail.com>
2018-03-19 00:09:33 +0000

    moving stuff in ch9 adding ch10

---
+++ Chapter10/DemoSchematics/src/app/jedis/jedis.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jedis',
+  templateUrl: './jedis.component.html',
+  styleUrls: ['./jedis.component.css']
+})
+export class JedisComponent implements OnInit {
+
+  constructor(private store: Store<any>) { } }
+
+  ngOnInit() {
+  }
+
+}
--- Chapter10/Entity/src/app/app-state.ts
+++ Chapter10/Entity/src/app/app-state.ts
@@ -1,5 +1,9 @@
 import { User } from "./user.model";
 
 export interface AppState {
-  user: User;
+  users: State;
+}
+
+interface State {
+  entities;
 }
