3f53d9164a66dde9df23ee70c805a6fe3385de57
Christoffer Noring <noringc@gmail.com>
2018-03-22 10:42:27 +0000

    fixing errors

--- Chapter10/DemoSchematics/src/app/todo-list/todo-list.component.ts
+++ Chapter10/DemoSchematics/src/app/todo-list/todo-list.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-todo-list',
-  templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.css']
+  selector: "app-todo-list",
+  templateUrl: "./todo-list.component.html",
+  styleUrls: ["./todo-list.component.css"]
 })
-export class TodoListComponent implements OnInit {
-
-  constructor(private store: Store<any>) { } }
-
-  ngOnInit() {
-  }
-
+export class TodoListComponent {
+  constructor(private store: Store<any>) {}
 }
